b6a16b3c38a2e8f4e8970e787bb52c013484173f
Yurii Sych <sichyuriyy@gmail.com>
2025-12-14 15:20:09 -0800

    add Macedonian Dynasty features

--- src/components/civilization-specific-feautres/macedonian-dynasty-features/MacedonianDynastyFeatures.tsx
+++ src/components/civilization-specific-feautres/macedonian-dynasty-features/MacedonianDynastyFeatures.tsx
@@ -1,7 +1,24 @@
 import shared from "../../../shared.module.css";
+import {useAppDispatch, useAppSelector} from "../../../hooks";
+import UpgradeItem from "../../upgrade-item/UpgradeItem";
+import grandWineryIcon from "../../../icons/grand-winery-1.png";
+import GatheringRateModifierId from "../../../data/gathering-rate-modifiers/GatheringRateModifierId";
+import {toggleGatheringRateModifier} from "../../../state/GatheringRateModifiersSlice";
 
 function MacedonianDynastyFeatures() {
-    return <div className={shared.civilizationSpecificFeatures}>Macedonian Dynasty features placeholder</div>;
+    const dispatch = useAppDispatch();
+    const gatheringRateModifiers: { [key: string]: boolean } = useAppSelector((rootState) => rootState.gatheringRateModifiers);
+
+    let grandWinery = <UpgradeItem icon={grandWineryIcon}
+                                   tooltip={{header: 'Grand Winery', text: 'Careful, assumes all food villagers in aura radius'}}
+                                   selected={gatheringRateModifiers[GatheringRateModifierId.MACEDONIAN_DYNASTY_GRAND_WINERY]}
+                                   onClick={() => dispatch(toggleGatheringRateModifier(GatheringRateModifierId.MACEDONIAN_DYNASTY_GRAND_WINERY))}/>
+
+    return (
+        <div className={shared.civilizationSpecificFeatures}>
+            {grandWinery}
+        </div>
+    );
 }
 
 export default MacedonianDynastyFeatures;
--- src/data/civilization-modifiers/MacedonianDynastyModifiers.ts
+++ src/data/civilization-modifiers/MacedonianDynastyModifiers.ts
@@ -1,9 +1,22 @@
 import CivilizationModifiers from "../../model/CivilizationModifiers";
 import CivilizationsEnum from "../CivilizationsEnum";
+import GatheringRateModifier from "../../model/GatheringRateModifier";
+import GatheringRateModifierId from "../gathering-rate-modifiers/GatheringRateModifierId";
+
+const grandWinery: GatheringRateModifier = {
+    id: GatheringRateModifierId.MACEDONIAN_DYNASTY_GRAND_WINERY,
+    apply: gatheringRates => ({
+        ...gatheringRates,
+        food: gatheringRates.food * 1.23
+    }),
+    description: 'food * 1.23'
+}
 
 const MACEDONIAN_DYNASTY_MODIFIERS: CivilizationModifiers = {
     civilization: CivilizationsEnum.MACEDONIAN_DYNASTY,
-    allGatheringRateModifiers: {},
+    allGatheringRateModifiers: {
+        [GatheringRateModifierId.MACEDONIAN_DYNASTY_GRAND_WINERY]: grandWinery
+    },
     defaultGatheringRateModifiers: [],
     allProductionSpeedModifiers: {},
     defaultProductionSpeedModifiers: [],
